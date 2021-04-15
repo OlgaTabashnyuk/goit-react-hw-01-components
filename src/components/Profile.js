3ca0f02653bff6987057e41b0451b0ea6b5e4f64
OlgaTabashnyuk <otabashniuk@outlook.com>
2021-04-16 00:12:29 +0300

    adds destructuring

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -8,28 +8,28 @@ const Profile = ({avatar, name, tag, location, stats}) => (
   <div className="profile">
       <div className="description">
           <img
-           src={user.avatar}
-           alt={user.name}
+           src={avatar}
+           alt={name}
            className="avatar"
            width="150"
          />
-         <p className="name">User name: {user.name}</p>
-         <p className="tag">User tag: @{user.tag}</p>
-         <p className="location">Location: {user.location}</p>
+         <p className="name">User name: {name}</p>
+         <p className="tag">User tag: @{tag}</p>
+         <p className="location">Location: {location}</p>
        </div>
      
        <ul className="stats">
          <li>
            <span className="label">Followers</span>
-           <span className="quantity"> {user.stats.followers}</span>
+           <span className="quantity"> {stats.followers}</span>
          </li>
          <li>
            <span className="label">Views</span>
-           <span className="quantity"> {user.stats.views}</span>
+           <span className="quantity"> {stats.views}</span>
          </li>
          <li>
            <span className="label">Likes</span>
-           <span className="quantity"> {user.stats.likes}</span>
+           <span className="quantity"> {stats.likes}</span>
          </li>
        </ul>
      </div>
